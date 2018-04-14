5b49c4816ffb334dccf266cad3caafa2e8fd8ca3
Mariusz <mariusz9dabal@gmail.com>
2018-04-14 13:18:55 +0100

    Working version 1.0

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-const container = document.querySelector('#container');
+//Etch a Sketch program by Jimmy
 
-for(let i = 0; i < 256; i++) {
-  const content = document.createElement('div');
-  content.classList.add('gridBox');
-  content.textContent = i;
-  container.appendChild(content);
+let squaresPerSide = 16;
+
+function makeSquares(squaresPerSide){
+  const container = document.querySelector('#container');
+  for(let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+    const content = document.createElement('div');
+    content.classList.add('gridBox');
+    container.appendChild(content);
+  }
+  container.setAttribute('style', `grid-template-columns: repeat(${squaresPerSide}, auto)`);
 }
+
+function removeSquares(){
+  container.innerHTML = '';
+}
+
+function reset(){
+  var btn = document.querySelector('#reset');
+  btn.addEventListener('click', () => {
+    removeSquares();
+    squaresPerSide = prompt("How many squares per side?");
+    makeSquares(squaresPerSide);
+    changeColor();
+  });
+}
+
+function changeColor() {
+  let grid = document.querySelectorAll('.gridBox');
+  grid.forEach((box) => {
+    box.addEventListener('mouseenter', (e) => {
+      box.classList.add('gridBoxHover');
+    });
+  });
+}
+
+makeSquares(squaresPerSide);
+changeColor();
+reset();
